a997c75fd915deb1720b3d5fef509708a1df04f9
David A <dajatuewo@gmail.com>
2020-07-16 18:48:56 -0500

    Bug Fix: Changed serilog.aspnetcore dependecy to native serilog.

--- site24x7-textformatter/Site24x7Formatter.cs
+++ site24x7-textformatter/Site24x7Formatter.cs
@@ -1,7 +1,7 @@
 ﻿namespace Serilog.Site24x7.TextFormatter
 {
-    using Serilog.Debugging;
     using Serilog.Events;
+    using Serilog.Formatting;
     using Serilog.Formatting.Json;
     using Serilog.Parsing;
     using System;
@@ -9,131 +9,127 @@
     using System.IO;
     using System.Linq;
 
-    public interface ISite24x7TextFormatter
+    public class Site24x7TextFormatter : ITextFormatter
     {
-        void FormatContent(LogEvent logEvent, TextWriter output);
-    }
+        private readonly JsonValueFormatter _valueFormatter;
 
-
-    public class Site24x7TextFormatter : ISite24x7TextFormatter
-    {
-        public void FormatContent(LogEvent logEvent, TextWriter output)
+        public Site24x7TextFormatter(JsonValueFormatter valueFormatter = null)
         {
+            _valueFormatter = valueFormatter ?? new JsonValueFormatter(typeTagName: "$type");
+        }
+
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            var buffer = new StringWriter();
+
             try
             {
-                if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
-                if (output == null) throw new ArgumentNullException(nameof(output));
-
-                output.Write("{\"_zl_timestamp\":\"");
-                output.Write(logEvent.Timestamp.ToUnixTimeMilliseconds());
-
-                output.Write("\",\"Level\":\"");
-                output.Write(logEvent.Level);
-
-                output.Write("\",\"MessageTemplate\":");
-                JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
-
-                output.Write(",\"RenderedMessage\":");
-
-                var message = logEvent.MessageTemplate.Render(logEvent.Properties);
-                JsonValueFormatter.WriteQuotedJsonString(message, output);
-
-                if (logEvent.Exception != null)
-                {
-                    output.Write(",\"Exception\":");
-                    JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
-                }
-
-                if (logEvent.Properties.Count != 0)
-                {
-                    WriteProperties(logEvent.Properties, output);
-                }
-
-                var tokensWithFormat = logEvent.MessageTemplate.Tokens
-                    .OfType<PropertyToken>()
-                    .Where(pt => pt.Format != null);
-
-                if (tokensWithFormat.Any())
-                {
-                    WriteRenderings(tokensWithFormat.GroupBy(pt => pt.PropertyName), logEvent.Properties, output);
-                }
-
-                output.Write('}');
+                FormatContent(logEvent, buffer);
             }
             catch (Exception ex)
             {
-                SelfLog.WriteLine(ex.Message);
+                Console.WriteLine(ex);
             }
+
+            output.WriteLine(buffer.ToString());
         }
 
-        private static void WriteProperties(
+        public static void FormatContent(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            output.Write("{\"_zl_timestamp\":\"");
+            output.Write(logEvent.Timestamp.ToUnixTimeMilliseconds());
+
+            output.Write("\",\"Level\":\"");
+            output.Write(logEvent.Level);
+
+            output.Write("\",\"MessageTemplate\":");
+            JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
+
+            output.Write(",\"RenderedMessage\":");
+
+            var message = logEvent.MessageTemplate.Render(logEvent.Properties);
+            JsonValueFormatter.WriteQuotedJsonString(message, output);
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"Exception\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
+            }
+
+            if (logEvent.Properties.Count != 0)
+            {
+                WriteProperties(logEvent.Properties, output);
+            }
+
+            var tokensWithFormat = logEvent.MessageTemplate.Tokens
+                .OfType<PropertyToken>()
+                .Where(pt => pt.Format != null);
+
+            if (tokensWithFormat.Any())
+            {
+                WriteRenderings(tokensWithFormat.GroupBy(pt => pt.PropertyName), logEvent.Properties, output);
+            }
+
+            output.Write('}');
+        }
+
+        public static void WriteProperties(
          IReadOnlyDictionary<string, LogEventPropertyValue> properties,
          TextWriter output)
         {
-            try
-            {
-                output.Write(",");
-                var precedingDelimiter = "";
+            output.Write(",");
+            var precedingDelimiter = "";
 
-                foreach (var property in properties)
-                {
-                    output.Write(precedingDelimiter);
-                    precedingDelimiter = ",";
-
-                    JsonValueFormatter.WriteQuotedJsonString(property.Key, output);
-                    output.Write(':');
-                    new JsonValueFormatter().Format(property.Value, output);
-                }
-            }
-            catch (Exception ex)
+            foreach (var property in properties)
             {
-                SelfLog.WriteLine(ex.Message);
+                output.Write(precedingDelimiter);
+                precedingDelimiter = ",";
+
+                JsonValueFormatter.WriteQuotedJsonString(property.Key, output);
+                output.Write(':');
+                new JsonValueFormatter().Format(property.Value, output);
             }
         }
 
-        private static void WriteRenderings(
+        public static void WriteRenderings(
            IEnumerable<IGrouping<string, PropertyToken>> tokensWithFormat,
            IReadOnlyDictionary<string, LogEventPropertyValue> properties,
            TextWriter output)
         {
-            try
+            output.Write(",\"Renderings\":{");
+
+            var rdelim = "";
+            foreach (var ptoken in tokensWithFormat)
             {
-                output.Write(",\"Renderings\":{");
+                output.Write(rdelim);
+                rdelim = ",";
 
-                var rdelim = "";
-                foreach (var ptoken in tokensWithFormat)
+                JsonValueFormatter.WriteQuotedJsonString(ptoken.Key, output);
+                output.Write(":[");
+
+                var fdelim = "";
+                foreach (var format in ptoken)
                 {
-                    output.Write(rdelim);
-                    rdelim = ",";
+                    output.Write(fdelim);
+                    fdelim = ",";
 
-                    JsonValueFormatter.WriteQuotedJsonString(ptoken.Key, output);
-                    output.Write(":[");
+                    output.Write("{\"Format\":");
+                    JsonValueFormatter.WriteQuotedJsonString(format.Format, output);
 
-                    var fdelim = "";
-                    foreach (var format in ptoken)
-                    {
-                        output.Write(fdelim);
-                        fdelim = ",";
-
-                        output.Write("{\"Format\":");
-                        JsonValueFormatter.WriteQuotedJsonString(format.Format, output);
-
-                        output.Write(",\"Rendering\":");
-                        var sw = new StringWriter();
-                        format.Render(properties, sw);
-                        JsonValueFormatter.WriteQuotedJsonString(sw.ToString(), output);
-                        output.Write('}');
-                    }
-
-                    output.Write(']');
+                    output.Write(",\"Rendering\":");
+                    var sw = new StringWriter();
+                    format.Render(properties, sw);
+                    JsonValueFormatter.WriteQuotedJsonString(sw.ToString(), output);
+                    output.Write('}');
                 }
 
-                output.Write('}');
+                output.Write(']');
             }
-            catch (Exception ex)
-            {
-                SelfLog.WriteLine(ex.Message);
-            }
+
+            output.Write('}');
         }
     }
 }
